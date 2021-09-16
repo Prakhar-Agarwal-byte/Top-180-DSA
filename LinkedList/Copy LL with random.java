fca0bec51234f81ad0dee9cdbd35db3102e236a0
Prakhar Agarwal <prakhar@everything.net.in>
2021-09-16 20:26:40 +0530

    Create Copy LL with random.java

---
+++ LinkedList/Copy LL with random.java
@@ -0,0 +1,30 @@
+class Solution {
+  public Node copyRandomList(Node head) {
+    Node cur = head;
+    while (cur != null) {
+      Node dCp = new Node(cur.val);
+      Node next = cur.next;
+      cur.next = dCp;
+      dCp.next = next;
+      cur = next;
+    }
+    cur = head;
+    while (cur != null) {
+      if (cur.random != null) {
+        cur.next.random = cur.random.next;
+      }
+      cur = cur.next.next;
+    }
+    cur = head;
+    Node d = new Node(0);
+    Node dCur = d;
+    while (cur != null) {
+      dCur.next = cur.next;
+      dCur = dCur.next;
+      Node next = cur.next.next;
+      cur.next = next;
+      cur = next;
+    }
+    return d.next;
+  }
+}
